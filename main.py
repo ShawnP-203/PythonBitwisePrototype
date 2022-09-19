a1c3b73c99a49979088959a94bb04b3dd30dedf3
ShawnP-203 <112486913+ShawnP-203@users.noreply.github.com>
2022-09-19 12:21:09 -0500

    improved handling of oversized integers

--- main.py
+++ main.py
@@ -1,53 +1,58 @@
 # Shawn Plackiyil
-# 9-16-2022
-
+# 9-16-2022 (last update 9-19-2022)
 # Credit to Wikipedia for instructions on subtraction and especially addition. See these URLs:
 # https://en.wikipedia.org/wiki/Special:PermanentLink/1094113375#Example:_a_simple_addition_program
 # https://en.wikipedia.org/wiki/Special:PermanentLink/1110485797#Subtraction
-
 # Arithmeticless Math
-# Adds and subtracts without any arithmetic operators.
+# Adds and subtracts without any arithmetic operators (assumes == and != are okay).
 # There are two known limitations:
 # 1. Can only use positive integers
 # 2. The second integer must be higher than the first
-class Binary(): # Program reuses a LOT of variables between methods, which is why I use a class
+class Binary(): # Program reuses a LOT of variables between methods, which is why I use a class.
   def __init__(self, integer1 = 0, integer2 = 0, finalSum = 0, sumInvertedSecond = 0, finalDifference = 0):
     self.integer1 = integer1
     self.integer2 = integer2
     self.finalSum = finalSum
     self.sumInvertedSecond = sumInvertedSecond
     self.finalDifference = finalDifference
-  def __positiveIntInput(self, question): # Leading underscores = do not call method outside of the class
-    """Asks for a positive integer and repeats the question if the user inputs otherwise"""
-    try:
-      assignTo = int(input(question).strip().replace(",", ""))
-      if "b" not in bin(assignTo)[2:]: return assignTo # Negative binaries have "b" in them
-      else:
-        print("Integer may not be negative.")
-        return 0 # There is a reason for invalid input getting a return value here —
-                 # because otherwise it would evaluate to "NoneType" which causes a TypeError
-                 # in the greaterThan() method.
-    except ValueError:
-      print("Please enter a *positive integer* (no negatives or non-numeric characters).")
-      return 0
-  def questions(self):
-    """Run with no arguments to see this program's questions"""
-    def __greaterThan(expr1, expr2): # Assuming that >, >=, <, and <= count as arithmetic operators
+  def __greaterThan(self, expr1, expr2): # Assuming that >, >=, <, and <= count as arithmetic operators
       """Returns True if expr2 is greater than expr1; False if vice versa"""
+      # General notes:
+      # bin() converts numbers to binary.
+      # [2:] truncates annoying "0b"s from their beginnings.
+      # .zfill() adds zeros to their beginnings until they reach a certain length (128 bits here).
+      # .ljust() adds choosable characters to their ENDS until they reach a certain length.
       expr1 = bin(expr1)[2:].zfill(128)
       expr2 = bin(expr2)[2:].zfill(128)
-      # bin() converts numbers to binary
-      # [2:] truncates an annoying "0b" from the start
-      # .zfill() pads the binaries with zeros until they reach a certain length (128 bits here).
       for bitCount, bits in enumerate(expr1):
         if expr1[bitCount] == "0" and expr2[bitCount] == "1": return True
         elif expr1[bitCount] == "1" and expr2[bitCount] == "0": return False
+  def __positiveIntInput(self, question, callIteration): # Leading underscores = do not call method outside of the class
+    """Asks for a positive integer and repeats the question if the user inputs otherwise"""
+    try:
+      assignTo = int(input(question).strip().replace(",", ""))
+      # .strip().replace(",", "") removes whitespace and commas from the input.
+      if "b" in bin(assignTo)[2:]:
+        print("Integer may not be negative.") # Negative binaries have "b" in them
+      #                       The code below equals 2 ** 127 - 1
+      elif self.__greaterThan(int("0".ljust(128, "1"), base = 2), assignTo) and callIteration == 1 \
+      or   self.__greaterThan(int("1".ljust(128, "0"), base = 2), assignTo) and callIteration == 2: 
+        print("Integer is too big.")
+      else: return assignTo # If input is acceptable
+    except ValueError:
+      print("Please enter a *positive integer* (no negatives or non-numeric characters).")
+    return 0 # There is a reason for invalid input getting a return value here —
+             # because otherwise it would evaluate to "NoneType" which causes a TypeError
+             # in the greaterThan() method.
+  def questions(self):
+    """Run with no arguments to see this program's questions"""
     while not self.integer1:
-      self.integer1 = self.__positiveIntInput("Enter a positive integer: ")
+      self.integer1 = self.__positiveIntInput("Enter a positive integer: ", 1)
     while True:
-      self.integer2 = self.__positiveIntInput("Enter an integer bigger than {:,}: ".format(self.integer1))
+      self.integer2 = self.__positiveIntInput("Enter an integer bigger than {:,}: " \
+                                              .format(self.integer1), 2)
       if self.integer2 != 0:
-        if __greaterThan(self.integer1, self.integer2): break
+        if self.__greaterThan(self.integer1, self.integer2): break
   def calculateSumDifference(self, integer1Bin, integer2Bin):
     """Converts two integers to binary and calculates the sum and difference"""
     integer1Bin = int(bin(self.integer1)[2:], base = 2) # Potentially important retrospective note:
